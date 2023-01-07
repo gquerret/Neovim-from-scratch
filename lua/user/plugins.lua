9442a0ca3d1d2d67153ffac9e6fbd944ccc85597
Gilles Querret <g.querret@riverside-software.fr>
2023-01-07 12:43:51 +0100

    Commit ID

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -74,7 +74,7 @@ return packer.startup(function(use)
   use { "rafamadriz/friendly-snippets", commit = "2be79d8a9b03d4175ba6b3d14b082680de1b31b1" } -- a bunch of snippets to use
 
 	-- LSP
-	use({ "gquerret/nvim-lspconfig" }) -- enable LSP
+	use({ "gquerret/nvim-lspconfig", commit = "6f59b8452bcbe766e8153347ba5ee17e22af56a9" }) -- enable LSP
   use { "williamboman/mason.nvim", commit = "c2002d7a6b5a72ba02388548cfaf420b864fbc12"} -- simple to use language server installer
   use { "williamboman/mason-lspconfig.nvim", commit = "0051870dd728f4988110a1b2d47f4a4510213e31" }
 	use { "jose-elias-alvarez/null-ls.nvim", commit = "c0c19f32b614b3921e17886c541c13a72748d450" } -- for formatters and linters
